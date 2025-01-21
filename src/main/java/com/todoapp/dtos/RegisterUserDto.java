0928a8c1ac2fa392b0342139a522a320186abb45
Miten Patel <patelmit3n@gmail.com>
2025-01-20 20:50:47 -0500

    remove unused imports

--- src/main/java/com/todoapp/dtos/RegisterUserDto.java
+++ src/main/java/com/todoapp/dtos/RegisterUserDto.java
@@ -2,7 +2,6 @@ package com.todoapp.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
