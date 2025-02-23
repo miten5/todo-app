cc1257f4a98babe83bbd809c97e4b1453285d23d
Miten Patel <patelmit3n@gmail.com>
2025-02-23 13:16:33 -0500

    add CrossOrigin

--- src/main/java/com/todoapp/controller/AuthenticationController.java
+++ src/main/java/com/todoapp/controller/AuthenticationController.java
@@ -8,11 +8,9 @@ import com.todoapp.service.AuthenticationService;
 import com.todoapp.service.JwtService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RequestMapping("/auth")
 @RestController
 public class AuthenticationController {
